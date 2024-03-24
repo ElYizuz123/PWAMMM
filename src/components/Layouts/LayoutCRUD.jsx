a7f395879ce6d26e2057dcb0b58e6ab822bcc8ac
ElYizuz123 <jose.jesus.zarate.garcia@outlook.com>
2024-03-24 14:00:57 -0600

    changes in font

--- src/components/Layouts/LayoutCRUD.jsx
+++ src/components/Layouts/LayoutCRUD.jsx
@@ -1,9 +1,14 @@
+import { League_Gothic } from "next/font/google"
+
+const league_gothic = League_Gothic({ subsets: ['latin'] })
 
 const LayoutCRUD = ({ children, title }) => {
     return (
         <div className="flex justify-start">
             <div className="h-screen w-48 bg-[#FF533A] bg-opacity-30 rounded-tl-3xl rounded-br-3xl flex flex-col">  
-                <h1 className="text-center text-black opacity text-lg">MUJERES MEZCALERAS DE MICHOACÁN</h1>
+                <div className={league_gothic.className}>
+                    <h1 className="text-center text-black opacity text-lg">MUJERES MEZCALERAS DE MICHOACÁN</h1>
+                </div>
             </div>
             <div className="w-full">
                 <div className="w-full h-16 bg-[#D9D9D9] flex justify-between items-center p-10">
@@ -15,11 +20,11 @@ const LayoutCRUD = ({ children, title }) => {
                     </div>
                     <div className="flex justify-center">
                         <p className="font-bold">Administrador</p>
-                        <div class="bg-black w-[0.2%] opacity-50 ml-4 mr-4"></div>
+                        <div className="bg-black w-[0.2%] opacity-50 ml-4 mr-4"></div>
                         <button>
                             <img src="/notification.png" className="w-7 h-"></img>    
                         </button>
-                        <div class="bg-black w-[0.2%] opacity-50 ml-4 mr-4"></div>
+                        <div className="bg-black w-[0.2%] opacity-50 ml-4 mr-4"></div>
                         <button>
                             <img src="/logout.png" className="w-7 h-7"></img>
                         </button>
