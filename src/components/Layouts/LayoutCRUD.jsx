db41ac4ca2d66e30644c7ddd8310d225f1689ab5
ElYizuz123 <jose.jesus.zarate.garcia@outlook.com>
2024-03-24 12:58:04 -0600

    icons added

--- src/components/Layouts/LayoutCRUD.jsx
+++ src/components/Layouts/LayoutCRUD.jsx
@@ -2,19 +2,27 @@
 const LayoutCRUD = ({ children, title }) => {
     return (
         <div className="flex justify-start">
-            <div className="h-screen w-48 bg-[#FF533A] opacity-30 rounded-tl-3xl rounded-br-3xl">
-
+            <div className="h-screen w-48 bg-[#FF533A] bg-opacity-30 rounded-tl-3xl rounded-br-3xl flex flex-col">  
+                <h1 className="text-center text-black opacity text-lg">MUJERES MEZCALERAS DE MICHOACÁN</h1>
             </div>
             <div className="w-full">
-                <div className="w-full h-16 bg-[#D9D9D9] flex justify-around items-center">
+                <div className="w-full h-16 bg-[#D9D9D9] flex justify-between items-center p-10">
                     <div>
-                        <div className="flex justify-center">
+                        <div className="flex justify-center ml-10">
                             <p>Tu perfil {'>'}</p>
-                            <p className="ml-1 text-[#C71E78] font-bold">Ventas</p>
+                            <p className="ml-1 text-[#C71E78] font-bold">{title}</p>
                         </div>
                     </div>
-                    <div>
+                    <div className="flex justify-center">
                         <p className="font-bold">Administrador</p>
+                        <div class="bg-black w-[0.2%] opacity-50 ml-4 mr-4"></div>
+                        <button>
+                            <img src="/notification.png" className="w-7 h-"></img>    
+                        </button>
+                        <div class="bg-black w-[0.2%] opacity-50 ml-4 mr-4"></div>
+                        <button>
+                            <img src="/logout.png" className="w-7 h-7"></img>
+                        </button>
                     </div>
                 </div>
                 {children}
