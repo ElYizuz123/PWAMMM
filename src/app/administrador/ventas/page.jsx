085ff436f2cb841ca993e2dd5436c97167e1bb0e
ElYizuz123 <jose.jesus.zarate.garcia@outlook.com>
2024-03-25 22:31:59 -0600

    tittle added

--- src/app/administrador/ventas/page.jsx
+++ src/app/administrador/ventas/page.jsx
@@ -10,11 +10,18 @@ const page = () => {
     <LayoutCRUD title="Ventas">
       <main className='flex flex-col items-center justify-between w-full h-full'>
         <div className='relative w-full h-full overflow-hidden'>
-            <div className='absolute -top-[60%] w-full'>
-              <Image src="/mezcal_background.png" alt="Imagen de fondo" width={1000} height={1000} objectFit='cover' className='w-full opacity-60'/>
+          <div className='absolute -top-[60%] w-full'>
+            <Image src="/mezcal_background.png" alt="Imagen de fondo" width={1000} height={1000} objectFit='cover' className='w-full opacity-50' />
+          </div>
+          <div className='w-full h-screen'>
+            <div className='w-full flex justify-center'>
+              <p className='mt-6 text-3xl font-bold'>Bienvenido,</p>
+              <p className='mt-6 ml-3 text-3xl font-bold text-[#F70073]'>Ventas</p>
             </div>
-            <div className='w-full h-screen '>
+            <div className='w-full h-full flex grid-flow-row justify-center'>
+              <div className='w-9/12 h-[0.3%] bg-black mt-10'/>
             </div>
+          </div>
         </div>
       </main>
     </LayoutCRUD>
