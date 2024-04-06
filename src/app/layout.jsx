cddaa684fe8515b4ed34bc15bd18d1346c514525
ElYizuz123 <jose.jesus.zarate.garcia@outlook.com>
2024-04-05 20:11:50 -0600

    Provider

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={k2d.className}>
-        {children}
+      <ProductProvider>
+          <div>{children}</div>
+        </ProductProvider>
         <footer>
           <Footer />
         </footer>
