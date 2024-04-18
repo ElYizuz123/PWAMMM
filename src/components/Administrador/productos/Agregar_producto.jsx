d35aec1fc66677be1189de0aec5eb9614535f5ea
ElYizuz123 <jose.jesus.zarate.garcia@outlook.com>
2024-04-18 17:30:41 -0600

    producto form improved

--- src/components/Administrador/productos/Agregar_producto.jsx
+++ src/components/Administrador/productos/Agregar_producto.jsx
@@ -3,70 +3,76 @@ import React, { useEffect, useRef, useState } from 'react'
 import Crear_Producto from './Crear_Producto'
 import Image from 'next/image'
 import Crear_Acompanamiento from './Crear_Acompanamiento'
-
+import Modal from 'react-modal'
 const Agregar_producto = ({ marcas }) => {
-
-    const [cProductIsOpen, setCProductIsOpen] = useState(false)
-    const [cAcompIsOpen, setCAcompIsOpen] = useState(false)
-    const createRef = useRef(null)
-    const createRefAcomp = useRef(null)
+    const [botellaIsOpen, setBotellaIsOpen] = useState(false)
+    const [acompanIsOpen, setAcompanIsOpen] = useState(false)
 
     //Función para abrir pop-up crear productos
-    const openCProduct = () => {
-        setCProductIsOpen(true)
-    };
 
-    //Función para cerrar pop-up crear productos
-    const closeCProduct = () => {
-        setCProductIsOpen(false)
-    };
+    function openBotella() {
+        setBotellaIsOpen(true);
+    }
 
-    //Función para abrir pop-up crear productos
-    const openCAcomp = () => {
-        setCAcompIsOpen(true)
-    };
+    function closeBotella() {
+        setBotellaIsOpen(false);
+    }
 
-    //Función para cerrar pop-up crear productos
-    const closeCAcomp = () => {
-        setCAcompIsOpen(false)
-    };
+    function openAcompan() {
+        setAcompanIsOpen(true);
+    }
 
-    //Scroll automático a ventana emergente
-    useEffect(() => {
-        if (cProductIsOpen) {
-            createRef.current.scrollIntoView({ behavior: 'smooth' })
-        }
-    }, [cProductIsOpen])
+    function closeAcompan() {
+        setAcompanIsOpen(false);
+    }
 
-    useEffect(() => {
-        if (cAcompIsOpen) {
-            createRefAcomp.current.scrollIntoView({ behavior: 'smooth' })
-        }
-    }, [cAcompIsOpen])
-
+    const customStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: '60%',
+          bottom: '50%',
+          marginRight: '-50%',
+          marginBottom: '-50%',
+          transform: 'translate(-50%, -50%)',
+          backgroundColor: '#00000000',
+          border: 'none',
+          boxShadow: 'none',
+          overflow:'auto',
+          scrollbarWidth: 'none',
+          msOverflowStyle: 'none'
+        },
+      };
 
     return (
         <div>
-            <div hidden={!cProductIsOpen} ref={createRef} className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-6/12 h-[670px] ${cProductIsOpen ? "" : "pointer-events-none"}`}>
-                {cProductIsOpen && <Crear_Producto
-                    isOpen={cProductIsOpen}
-                    onClose={closeCProduct}
+            <Modal
+                isOpen={botellaIsOpen}
+                onRequestClose={closeBotella}
+                style={customStyles}
+            >
+                <Crear_Producto 
+                    onClose={closeBotella}
                     marcas={marcas}
-                />}
-            </div>
-            <div hidden={!cAcompIsOpen} ref={createRefAcomp} className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-6/12 h-[700px] ${cAcompIsOpen ? "" : "pointer-events-none"}`}>
-                {cAcompIsOpen && <Crear_Acompanamiento
-                    isOpen={cAcompIsOpen}
-                    onClose={closeCAcomp}
+                />
+            </Modal>
+            <Modal
+                isOpen={acompanIsOpen}
+                onRequestClose={closeAcompan}
+                style={customStyles}
+            >
+                <Crear_Acompanamiento 
+                    onClose={closeAcompan}
                     marcas={marcas}
-                />}
-            </div>
+                />
+            </Modal>
+            
             <div className='flex '>
-                <button onClick={openCProduct} className='bg-[#98E47D] w-44 h-10 font-bold rounded-lg flex justify-between items-center hover:bg-[#98e47dab]'>
+                <button onClick={openBotella} className='bg-[#98E47D] w-44 h-10 font-bold rounded-lg flex justify-between items-center hover:bg-[#98e47dab]'>
                     <Image alt="mas" layout='intrinsic' width={40} height={40} src='/emoticons/plus.png' className='w-8 ml-2' />
                     <p className='mr-3'>Agregar botella</p>
                 </button>
-                <button onClick={openCAcomp} className='bg-[#98E47D] w-64 h-10 font-bold rounded-lg flex justify-between items-center hover:bg-[#98e47dab] ml-5'>
+                <button onClick={openAcompan} className='bg-[#98E47D] w-64 h-10 font-bold rounded-lg flex justify-between items-center hover:bg-[#98e47dab] ml-5'>
                     <Image alt="mas" layout='intrinsic' width={40} height={40} src='/emoticons/plus.png' className='w-8 ml-2' />
                     <p className='mr-3'>Agregar acompañamiento</p>
                 </button>
