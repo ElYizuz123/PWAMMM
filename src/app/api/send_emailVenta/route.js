0259c4e954798114d7e7f6c4d388ae59d303e2e2
ElYizuz123 <jose.jesus.zarate.garcia@outlook.com>
2024-05-08 19:58:53 -0600

    send email changes

--- src/app/api/send_emailVenta/route.js
+++ src/app/api/send_emailVenta/route.js
@@ -1,26 +1,32 @@
+import { Resend } from "resend";
 
+const resend = new Resend("re_g8h7iK1P_B4mJ6DE87iJ86au9R8yR724w");
 
-  import { NextResponse } from "next/server";
-
-const nodemailer = require("nodemailer");
-export const revalidate = 0;
 export async function POST(request) {
   try {
-   const { total, metodoPago, metodoEnvio, 
-    nombreCliente, apellidoCliente, nombreEmpresa, telefono, correo, 
-    calle, colonia, ciudad, cp, numExterior, numInterior, pais,productos} = await request.json();
+    const data = await request.json();
+    const {
+      productos,
+      metodoEnvio,
+      total,
+      metodoPago,
+      nombreCliente,
+      apellidoCliente,
+      telefono,
+      correo,
+      numInterior,
+      nombreEmpresa,
+      pais,
+      ciudad,
+      colonia,
+      cp,
+      calle,
+      numExterior,
+    } = data;
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: "aguimtz.2003@gmail.com",
-        pass: "dwgt bnzs xygg bqpm",
-      },
-    });
-    
-    const productosHTML =  productos.map((producto) => `
+    const productosHTML = productos
+      .map(
+        (producto) => `
      
       
         <li>
@@ -31,14 +37,15 @@ export async function POST(request) {
          </li>
     
      
-    `).join('') ;
+    `
+      )
+      .join("");
 
-    // Construir el HTML completo para el correo electrónico
-    const mailOption = {
-      from: "Pagína web <aguimtz.2003@gmail.com>",
+    const emailResponse = await resend.emails.send({
+      from: "PAGINA MUJERES MEZCALERAS <onboarding@resend.dev>",
       to: "aguimtz.2003@gmail.com",
-      subject: "NUEVA VENTA ",
-      //hace un mapeo peroooo ya vi, no esta igualando los productos con la información osea no se de donde los saca
+      subject: "NUEVA VENTA",
+
       html: `
         <h2 style="color: #F70073;">Detalles del Pedido</h2>
         <ul>
@@ -53,7 +60,11 @@ export async function POST(request) {
           <li><strong>Nombre:</strong> ${nombreCliente} ${apellidoCliente}</li>
           <li><strong>Teléfono:</strong> ${telefono}</li>
           <li><strong>Correo:</strong> ${correo}</li>
-          ${numInterior === " " ? `<li><strong>Nombre empresa:</strong> ${nombreEmpresa}</li>`: ""}
+          ${
+            numInterior === " "
+              ? `<li><strong>Nombre empresa:</strong> ${nombreEmpresa}</li>`
+              : ""
+          }
         </ul>
        <h3 style="color:#F70073;">Datos de Dirección</h3>
         <ul>
@@ -64,15 +75,24 @@ export async function POST(request) {
           <li><strong>CP:</strong> ${cp}</li>
           <li><strong>Calle:</strong> ${calle}</li>
           <li><strong>Numero:</strong>  #${numExterior}</li>
-        ${numInterior === null ? " " :  `<li><strong>Numero interior:</strong> #${numInterior}</li>`}
+        ${
+          numInterior === null
+            ? " "
+            : `<li><strong>Numero interior:</strong> #${numInterior}</li>`
+        }
         </ul>
-      `
-    };
+      `,
+    });
 
-    await transporter.sendMail(mailOption)
-
-    return NextResponse.json({ message: "Se han guardado sus datos." }, { status: 200 })
+    return new Response(JSON.stringify({ message: "Email enviado" }), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
   } catch (error) {
-    return NextResponse.json({ message: "Ha ocurrido un error al enviar el correo." }, { status: 500 })
+    // Si ocurrió un error, devuelve una respuesta de error
+    return new Response(JSON.stringify({ error: "Error en email" }), {
+      headers: { "Content-Type": "application/json" },
+      status: 500,
+    });
   }
 }
